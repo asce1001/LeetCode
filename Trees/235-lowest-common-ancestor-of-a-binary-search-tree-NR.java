9617e7e1375456c7bb2cf25342cc67c1ed626891
Yatindra Mahadik <55409963+asce1001@users.noreply.github.com>
2024-08-24 23:47:51 -0400

    code clean-up

--- Trees/235-lowest-common-ancestor-of-a-binary-search-tree-NR.java
+++ Trees/235-lowest-common-ancestor-of-a-binary-search-tree-NR.java
@@ -1,13 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         while(true){
@@ -15,6 +5,5 @@ class Solution {
             else if(p.val < root.val && q.val < root.val) root = root.left;
             else return root;
         }
-        
     }
 }

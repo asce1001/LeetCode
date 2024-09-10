ac53bc454ac5e0d742c5b4dd3cd3a648d1ee20d3
Yatindra Mahadik <55409963+asce1001@users.noreply.github.com>
2024-09-10 10:39:09 -0400

    3 Sum brute force

---
+++ 3 sum 2 sum/15-3SumBF.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        int n = nums.length;
+        for(int i = 0; i < n - 2; i++){
+            for(int j = i + 1; j < n - 1; j++){
+                for(int k = j + 1; k < n; k++){
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        int[] arr = new int[]{nums[i], nums[j], nums[k]};
+                        Arrays.sort(arr);
+                        result.add(new ArrayList<>(Arrays.stream(arr).boxed().collect(Collectors.toCollection(ArrayList::new))));
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+}

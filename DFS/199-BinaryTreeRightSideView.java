132742a4e66fb1a124f51451093aae6ab216a34c
Yatindra Mahadik <55409963+asce1001@users.noreply.github.com>
2024-10-15 22:41:36 -0400

    Update 199-BinaryTreeRightSideView.java

--- DFS/199-BinaryTreeRightSideView.java
+++ DFS/199-BinaryTreeRightSideView.java
@@ -14,26 +14,20 @@
  * }
  */
 class Solution {
-    List<Integer>  result = new ArrayList<>();
+    List<Integer> result;
     public List<Integer> rightSideView(TreeNode root) {
+        result = new ArrayList<>();
         if(root == null) return result;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i = 0; i < size; i++){
-                TreeNode curr = q.poll();
-                if(i == size - 1){
-                    result.add(curr.val);
-                }
-                if(curr.left != null){
-                    q.add(curr.left);
-                }
-                if(curr.right != null){
-                    q.add(curr.right);
-                }
-            }
-        }
+        helper(root, 0);
         return result;
+    }
+
+    private void helper(TreeNode root, int level){
+        if(root == null) return;
+        if(result.size() == level){
+            result.add(root.val);
+        }
+        helper(root.right, level +1);
+        helper(root.left, level + 1);
     }
 }

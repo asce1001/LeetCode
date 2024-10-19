f0ddf16d9ef2018e29351bbb522abed70eaacb5f
Yatindra Mahadik <55409963+asce1001@users.noreply.github.com>
2024-10-18 23:12:34 -0400

    Update 993-CousinsInBinaryTree.java

--- BFS/993-CousinsInBinaryTree.java
+++ BFS/993-CousinsInBinaryTree.java
@@ -1,36 +1,47 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-        q = deque()
-        if not root: return False
-        q.append(root)
-        xFlag = False
-        yFlag = False
-        while q:
-            size = len(q)
-            for i in range(size):
-                curr = q.popleft()
-                if curr.right is not None and curr.left is not None:
-                    if curr.right.val == x and curr.left.val == y: return False
-                    if curr.right.val == y and curr.left.val == x: return False
-                if curr.val == x: xFlag = True
-                if curr.val == y: yFlag = True
-                if curr.left: q.append(curr.left)
-                if curr.right: q.append(curr.right)
-            if xFlag and yFlag: return True
-            if xFlag or yFlag: return False
-        return False
-
-
-
-
-
-
-
-
-        
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        boolean xFlag = false;
+        boolean yFlag = false;
+        if(root == null) return true;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            //boolean xFlag = false; 
+            //boolean yFlag = false; 
+            for(int i = 0; i < size; i++){
+                TreeNode curr = q.poll();
+                if(curr.val == x) xFlag = true;
+                if(curr.val == y) yFlag = true;
+                if(curr.left != null && curr.right != null){
+                    if(curr.left.val == x && curr.right.val == y) return false;
+                    if(curr.right.val == x && curr.left.val == y) return false;
+                }
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right != null){
+                    q.add(curr.right);
+                }
+            }
+            if(xFlag && yFlag) return true;
+            if(xFlag || yFlag) return false;
+        }
+        return false;
+    }
+}

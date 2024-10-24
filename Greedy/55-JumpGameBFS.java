f2d98d7d483d27f73effc5421e891278495202b0
Yatindra Mahadik <55409963+asce1001@users.noreply.github.com>
2024-10-23 22:01:55 -0400

    Create 55-JumpGameBFS optimised

---
+++ Greedy/55-JumpGameBFS.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums == null || nums.length == 0 || nums.length < 2) return true;
+        Queue<Integer> q = new LinkedList<>();
+        Set<Integer> set = new HashSet<>();
+        q.add(0); set.add(0);
+        int n = nums.length - 1 ;
+        while(!q.isEmpty()){
+            int i = q.poll();
+            for(int j = 1; j <= nums[i]; j++){
+                int idx = i + j;
+                if(idx  == n) return true;
+                if(!set.contains(idx)){
+                    q.add(idx);
+                    set.add(idx);
+                }
+                
+            }
+        }
+        return false;
+    }
+}
